83219cfb0162c1cc9a38d244711538ea98d0c727
Anonymoose725 <ethangat725@gmail.com>
2026-07-25 23:13:03 -0400

    cmake config

---
+++ build/CMakeFiles/Lampara.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lampara.dir/main.cpp.o"
+  "CMakeFiles/Lampara.dir/main.cpp.o.d"
+  "Lampara"
+  "Lampara.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lampara.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Lampara.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Lampara.
+# This may be replaced when dependencies are built.
